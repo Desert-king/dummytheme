85f75f5ff455c89fea859f49990b118e5129cd1c
unknown <farhanamaysha29@gmail.com>
2022-03-04 15:27:44 +0600

    dynamic shop page with design

--- cplparts/template-parts/sidebar/shop-page-sidebar.php
+++ cplparts/template-parts/sidebar/shop-page-sidebar.php
@@ -1,3 +1,6 @@
+<?php 
+global $product;
+?>
 <div class="block-split__item block-split__item-sidebar col-auto">
     <div class="sidebar sidebar--offcanvas--mobile">
         <div class="sidebar__backdrop"></div>
@@ -127,11 +130,64 @@ C6.1,7.8,6.1,8.4,5.7,8.7z"
                         </div>
                         <!-- 11111111111111111111 -->
                         <?php 
+                            //    add_action('woocommerce_after_shop_loop_item','add_attributes', 3);
+                            //    function add_attributes() {
+                            //    global $product;
                                
+                            //    $product_attributes = array('pa_brand', 'pa_color', 'pa_vehicle');
+                            //    $attr_output = array();
+                            
+                            //    foreach( $product_attributes as $taxonomy ){
+                            //    if( taxonomy_exists($taxonomy) ){
+                            //    $label_name = get_taxonomy( $taxonomy )->labels->singular_name;
+                            //    $value1 = $product->get_attribute('pa_brand');
+                            //    $value2 = $product->get_attribute('pa_color');
+                            //    $value3 = $product->get_attribute('pa_vehicle');
+                            
+                               
+                            //    if( ! empty($value1) ){
+                           
+                            //    $attr_output[] = "<span class=”'.$taxonomy.'”>'.$label_name.': '.$value1.'</span>";
+                            //    }
+                            //    }
+                            //    }
+                               
+                              
+                            //    echo '<div class="product-attributes">'.implode( '<br>', $attr_output ).'</div>';
+                            //    }
+                        ?>
+                        <?php 
+                          add_shortcode( 'product_attributes', 'get_product_attributes' );
+                          function get_product_attributes() {
+                              $output = '<div style="list-style:inline-block;">';
+                              foreach( wc_get_attribute_taxonomies() as $attribute ) {
+                                  $taxonomy = 'pa_' . $attribute->attribute_name;
+                                  $term_names = get_terms( array( 'taxonomy' => $taxonomy, 'fields' => 'names' ) );
+                                  
+                                  $output .= '<div class="widget-filters__item">
+                                  <div class="filter filter--opened" data-collapse-item> <button type="button" class="filter__title" data-collapse-trigger>' . $attribute->attribute_name . '
+                                  <span class="filter__arrow">
+                                  <svg width="12px" height="7px">
+                                      <path
+                                          d="M0.286,0.273 L0.286,0.273 C-0.070,0.629 -0.075,1.204 0.276,1.565 L5.516,6.993 L10.757,1.565 C11.108,1.204 11.103,0.629 10.747,0.273 L10.747,0.273 C10.385,-0.089 9.796,-0.086 9.437,0.279 L5.516,4.296 L1.596,0.279 C1.237,-0.086 0.648,-0.089 0.286,0.273 Z"
+                                      />
+                                  </svg>
+                                  </span>
+                                  </button> 
+                                  <div class="filter__body" data-collapse-content>  
+                                       <div class="filter__container">
+                                            <div class="filter-vehicle">
+                                                 <ul class="filter-vehicle__list"><li class="filter-vehicle__item">'. implode( '<li class="filter-vehicle__item">
+                                                        <span class="filter-vehicle__item-title">', $term_names) .'</span></li></li></ul></div></div></div></div></div>';
+                              }
+                              return $output . '</div>';
+                          }
+                          
 
                         ?>
+                        <?php echo do_shortcode('[product_attributes]'); ?>
                         <!-- 11111111111111111111111 -->
-                        <div class="widget-filters__item">
+                        <!-- <div class="widget-filters__item">
                             <div class="filter filter--opened" data-collapse-item>
                                 <button type="button" class="filter__title" data-collapse-trigger>
                                     Vehicle
@@ -359,6 +415,7 @@ C6.1,7.8,6.1,8.4,5.7,8.7z"
                                 </div>
                             </div>
                         </div>
+                       
                         <div class="widget-filters__item">
                             <div class="filter filter--opened" data-collapse-item>
                                 <button type="button" class="filter__title" data-collapse-trigger>
@@ -742,7 +799,7 @@ C6.1,7.8,6.1,8.4,5.7,8.7z"
                                     </div>
                                 </div>
                             </div>
-                        </div>
+                        </div> -->
                     </div>
                     <div class="widget-filters__actions d-flex"><button class="btn btn-primary btn-sm">Filter</button> <button class="btn btn-secondary btn-sm">Reset</button></div>
                 </div>
--- cplparts/woocommerce/archive-product.php
+++ cplparts/woocommerce/archive-product.php
@@ -28,7 +28,25 @@ get_header( 'shop' );
  */
 do_action( 'woocommerce_before_main_content' );
 
+
 ?>
+<!-- <header class="woocommerce-products-header"> -->
+	<?php //if ( apply_filters( 'woocommerce_show_page_title', true ) ) : ?>
+		<!-- <h1 class="woocommerce-products-header__title page-title"> -->
+		<?php //woocommerce_page_title(); ?>
+	</h1>
+	<?php //endif; ?>
+
+	<?//php
+	/**
+	 * Hook: woocommerce_archive_description.
+	 *
+	 * @hooked woocommerce_taxonomy_archive_description - 10
+	 * @hooked woocommerce_product_archive_description - 10
+	 */
+	//do_action( 'woocommerce_archive_description' );
+	?>
+<!-- </header> -->
  <div class="block-split block-split--has-sidebar">
     <div class="container">
         <div class="block-split__row row no-gutters">
@@ -102,7 +120,7 @@ C0.4,4,0,3.6,0,3.2V0.8C0,0.4,0.4,0,0.8,0h14.4C15.6,0,16,0.4,16,0.8v2.4C16,3.6,15
 								<div class="view-options__select">
 									<label for="view-option-limit">Show:</label>
 									<select id="view-option-limit" class="form-control form-control-sm" name="">
-										<option value="">16</option>
+										<option value=""><?php woocommerce_result_count(); ?></option>
 									</select>
 								</div>
 							</div>
@@ -126,22 +144,143 @@ C0.4,4,0,3.6,0,3.2V0.8C0,0.4,0.4,0,0.8,0h14.4C15.6,0,16,0.4,16,0.8v2.4C16,3.6,15
 							</div>
 						</div>
 					   <!-- tab view section ends -->
-<header class="woocommerce-products-header">
-	<?php if ( apply_filters( 'woocommerce_show_page_title', true ) ) : ?>
-		<h1 class="woocommerce-products-header__title page-title"><?php woocommerce_page_title(); ?></h1>
-	<?php endif; ?>
+					   <!-- product loop -->
+					   <div class="products-view__list products-list products-list--grid--4" data-layout="grid" data-with-features="false">
+						<div class="products-list__head">
+							<div class="products-list__column products-list__column--image">Image</div>
+							<div class="products-list__column products-list__column--meta">SKU</div>
+							<div class="products-list__column products-list__column--product">Product</div>
+							<div class="products-list__column products-list__column--rating">Rating</div>
+							<div class="products-list__column products-list__column--price">Price</div>
+						</div>
+						<div class="products-list__content">
+						     <?php 
+							  
+								if ( have_posts() ) {
+									while ( have_posts() ) {
+										the_post(); 
+								?>
+							<div class="products-list__item">
+								<div class="product-card">
+									<div class="product-card__actions-list">
+										<button class="product-card__action product-card__action--quickview" type="button" aria-label="Quick view">
+											<svg width="16" height="16">
+												<path
+													d="M14,15h-4v-2h3v-3h2v4C15,14.6,14.6,15,14,15z M13,3h-3V1h4c0.6,0,1,0.4,1,1v4h-2V3z M6,3H3v3H1V2c0-0.6,0.4-1,1-1h4V3z
+M3,13h3v2H2c-0.6,0-1-0.4-1-1v-4h2V13z"
+												/>
+											</svg>
+										</button>
+										<button class="product-card__action product-card__action--wishlist" type="button" aria-label="Add to wish list">
+											<svg width="16" height="16">
+												<path
+													d="M13.9,8.4l-5.4,5.4c-0.3,0.3-0.7,0.3-1,0L2.1,8.4c-1.5-1.5-1.5-3.8,0-5.3C2.8,2.4,3.8,2,4.8,2s1.9,0.4,2.6,1.1L8,3.7
+l0.6-0.6C9.3,2.4,10.3,2,11.3,2c1,0,1.9,0.4,2.6,1.1C15.4,4.6,15.4,6.9,13.9,8.4z"
+												/>
+											</svg>
+										</button>
+										<button class="product-card__action product-card__action--compare" type="button" aria-label="Add to compare">
+											<svg width="16" height="16">
+												<path d="M9,15H7c-0.6,0-1-0.4-1-1V2c0-0.6,0.4-1,1-1h2c0.6,0,1,0.4,1,1v12C10,14.6,9.6,15,9,15z" />
+												<path d="M1,9h2c0.6,0,1,0.4,1,1v4c0,0.6-0.4,1-1,1H1c-0.6,0-1-0.4-1-1v-4C0,9.4,0.4,9,1,9z" />
+												<path d="M15,5h-2c-0.6,0-1,0.4-1,1v8c0,0.6,0.4,1,1,1h2c0.6,0,1-0.4,1-1V6C16,5.4,15.6,5,15,5z" />
+											</svg>
+										</button>
+									</div>
+									<div class="product-card__image">
+										<div class="image image--type--product">
+											<a href="product-full.html" class="image__body"><img class="image__tag" src="<?php echo get_template_directory_uri(); ?>/assets/images/products/product-1-245x245.jpg" alt="" /></a>
+										</div>
+										<div class="status-badge status-badge--style--success product-card__fit status-badge--has-icon status-badge--has-text">
+											<div class="status-badge__body">
+												<div class="status-badge__icon">
+													<svg width="13" height="13"><path d="M12,4.4L5.5,11L1,6.5l1.4-1.4l3.1,3.1L10.6,3L12,4.4z" /></svg>
+												</div>
+												<div class="status-badge__text">Part Fit for 2011 Ford Focus S</div>
+												<div class="status-badge__tooltip" tabindex="0" data-toggle="tooltip" title="Part&#x20;Fit&#x20;for&#x20;2011&#x20;Ford&#x20;Focus&#x20;S"></div>
+											</div>
+										</div>
+									</div>
+									<div class="product-card__info">
+										<div class="product-card__meta"><span class="product-card__meta-title">SKU:</span> 140-10440-B</div>
+										<div class="product-card__name">
+											<div>
+												<div class="product-card__badges">
+													<div class="tag-badge tag-badge--sale">sale</div>
+													<div class="tag-badge tag-badge--new">new</div>
+													<div class="tag-badge tag-badge--hot">hot</div>
+												</div>
+												<a href="product-full.html">Brandix Spark Plug Kit ASR-400</a>
+											</div>
+										</div>
+										<div class="product-card__rating">
+											<div class="rating product-card__rating-stars">
+												<div class="rating__body">
+													<div class="rating__star rating__star--active"></div>
+													<div class="rating__star rating__star--active"></div>
+													<div class="rating__star rating__star--active"></div>
+													<div class="rating__star rating__star--active"></div>
+													<div class="rating__star"></div>
+												</div>
+											</div>
+											<div class="product-card__rating-label">4 on 3 reviews</div>
+										</div>
+										<div class="product-card__features">
+											<ul>
+												<li>Speed: 750 RPM</li>
+												<li>Power Source: Cordless-Electric</li>
+												<li>Battery Cell Type: Lithium</li>
+												<li>Voltage: 20 Volts</li>
+												<li>Battery Capacity: 2 Ah</li>
+											</ul>
+										</div>
+									</div>
+									<div class="product-card__footer">
+										<div class="product-card__prices"><div class="product-card__price product-card__price--current">$19.00</div></div>
+										<button class="product-card__addtocart-icon" type="button" aria-label="Add to cart">
+											<svg width="20" height="20">
+												<circle cx="7" cy="17" r="2" />
+												<circle cx="15" cy="17" r="2" />
+												<path
+													d="M20,4.4V5l-1.8,6.3c-0.1,0.4-0.5,0.7-1,0.7H6.7c-0.4,0-0.8-0.3-1-0.7L3.3,3.9C3.1,3.3,2.6,3,2.1,3H0.4C0.2,3,0,2.8,0,2.6
+V1.4C0,1.2,0.2,1,0.4,1h2.5c1,0,1.8,0.6,2.1,1.6L5.1,3l2.3,6.8c0,0.1,0.2,0.2,0.3,0.2h8.6c0.1,0,0.3-0.1,0.3-0.2l1.3-4.4
+C17.9,5.2,17.7,5,17.5,5H9.4C9.2,5,9,4.8,9,4.6V3.4C9,3.2,9.2,3,9.4,3h9.2C19.4,3,20,3.6,20,4.4z"
+												/>
+											</svg>
+										</button>
+										<button class="product-card__addtocart-full" type="button">Add to cart</button>
+										<button class="product-card__wishlist" type="button">
+											<svg width="16" height="16">
+												<path
+													d="M13.9,8.4l-5.4,5.4c-0.3,0.3-0.7,0.3-1,0L2.1,8.4c-1.5-1.5-1.5-3.8,0-5.3C2.8,2.4,3.8,2,4.8,2s1.9,0.4,2.6,1.1L8,3.7
+l0.6-0.6C9.3,2.4,10.3,2,11.3,2c1,0,1.9,0.4,2.6,1.1C15.4,4.6,15.4,6.9,13.9,8.4z"
+												/>
+											</svg>
+											<span>Add to wishlist</span>
+										</button>
+										<button class="product-card__compare" type="button">
+											<svg width="16" height="16">
+												<path d="M9,15H7c-0.6,0-1-0.4-1-1V2c0-0.6,0.4-1,1-1h2c0.6,0,1,0.4,1,1v12C10,14.6,9.6,15,9,15z" />
+												<path d="M1,9h2c0.6,0,1,0.4,1,1v4c0,0.6-0.4,1-1,1H1c-0.6,0-1-0.4-1-1v-4C0,9.4,0.4,9,1,9z" />
+												<path d="M15,5h-2c-0.6,0-1,0.4-1,1v8c0,0.6,0.4,1,1,1h2c0.6,0,1-0.4,1-1V6C16,5.4,15.6,5,15,5z" />
+											</svg>
+											<span>Add to compare</span>
+										</button>
+									</div>
+								</div>
+							</div>
+							<?php 
+							 } // end while
+							} // end if
+							?>
+							
+						</div>
+					</div>
+					   <!-- product loop -->
+					   
 
-	<?php
-	/**
-	 * Hook: woocommerce_archive_description.
-	 *
-	 * @hooked woocommerce_taxonomy_archive_description - 10
-	 * @hooked woocommerce_product_archive_description - 10
-	 */
-	do_action( 'woocommerce_archive_description' );
-	?>
-</header>
 <?php
+
 if ( woocommerce_product_loop() ) {
 
 	/**
@@ -151,7 +290,7 @@ if ( woocommerce_product_loop() ) {
 	 * @hooked woocommerce_result_count - 20
 	 * @hooked woocommerce_catalog_ordering - 30
 	 */
-	do_action( 'woocommerce_before_shop_loop' );
+	//do_action( 'woocommerce_before_shop_loop' );
 
 	woocommerce_product_loop_start();
 
@@ -169,7 +308,53 @@ if ( woocommerce_product_loop() ) {
 	}
 
 	woocommerce_product_loop_end();
-
+    ?>
+	<!-- product loop -->
+	
+	<!-- product loop -->
+	<!-- pagination -->
+	<div class="block-split__item block-split__item-content col-auto">
+    
+	<div class="products-view__pagination">
+			<nav aria-label="Page navigation example">
+				<ul class="pagination">
+					<li class="page-item disabled">
+						<a class="page-link page-link--with-arrow" href="#" aria-label="Previous">
+							<span class="page-link__arrow page-link__arrow--left" aria-hidden="true">
+								<svg width="7" height="11">
+									<path d="M6.7,0.3L6.7,0.3c-0.4-0.4-0.9-0.4-1.3,0L0,5.5l5.4,5.2c0.4,0.4,0.9,0.3,1.3,0l0,0c0.4-0.4,0.4-1,0-1.3l-4-3.9l4-3.9C7.1,1.2,7.1,0.6,6.7,0.3z" />
+								</svg>
+							</span>
+						</a>
+					</li>
+					<li class="page-item"><a class="page-link" href="#">1</a></li>
+					<li class="page-item active" aria-current="page">
+						<span class="page-link">2 <span class="sr-only">(current)</span></span>
+					</li>
+					<li class="page-item"><a class="page-link" href="#">3</a></li>
+					<li class="page-item"><a class="page-link" href="#">4</a></li>
+					<li class="page-item page-item--dots"><div class="pagination__dots"></div></li>
+					<li class="page-item"><a class="page-link" href="#">9</a></li>
+					<li class="page-item">
+						<a class="page-link page-link--with-arrow" href="#" aria-label="Next">
+							<span class="page-link__arrow page-link__arrow--right" aria-hidden="true">
+								<svg width="7" height="11">
+									<path
+										d="M0.3,10.7L0.3,10.7c0.4,0.4,0.9,0.4,1.3,0L7,5.5L1.6,0.3C1.2-0.1,0.7,0,0.3,0.3l0,0c-0.4,0.4-0.4,1,0,1.3l4,3.9l-4,3.9
+	C-0.1,9.8-0.1,10.4,0.3,10.7z"
+									/>
+								</svg>
+							</span>
+						</a>
+					</li>
+				</ul>
+			</nav>
+			<div class="products-view__pagination-legend">Showing 6 of 98 products</div>
+		</div>
+		</div>
+		
+		<!-- pagination -->
+	<?php
 	/**
 	 * Hook: woocommerce_after_shop_loop.
 	 *
@@ -184,6 +369,9 @@ if ( woocommerce_product_loop() ) {
 	 */
 	do_action( 'woocommerce_no_products_found' );
 }
+?>
+
+<?php
 
 /**
  * Hook: woocommerce_after_main_content.
@@ -199,6 +387,7 @@ do_action( 'woocommerce_after_main_content' );
  */
 // do_action( 'woocommerce_sidebar' );
 ?>
+                       
 
                      </div>
                 </div>
