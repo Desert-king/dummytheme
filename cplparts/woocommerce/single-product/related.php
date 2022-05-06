e4a0cf7d23276b76cdc3130aabd82809086cdaeb
unknown <farhanamaysha29@gmail.com>
2022-05-06 16:00:10 +0600

    dynamic related product with scroll view

--- cplparts/woocommerce/single-product/related.php
+++ cplparts/woocommerce/single-product/related.php
@@ -23,56 +23,59 @@ if ( $related_products ) : ?>
 
 	<section class="related products">
 
-		<?php
-		$heading = apply_filters( 'woocommerce_product_related_products_heading', __( 'Related products', 'woocommerce' ) );
-
-		if ( $heading ) :
-			?>
-			<h2><?php echo esc_html( $heading ); ?></h2>
-		<?php endif; ?>
-
+		
 		<!-- new html -->
 		<div class="block-space block-space--layout--divider-nl"></div>
-                                <div class="block block-products-carousel" data-layout="grid-5">
-                                    <div class="container">
-                                        <div class="section-header">
-                                            <div class="section-header__body">
-                                                <h2 class="section-header__title">Related Products</h2>
-                                                <div class="section-header__spring"></div>
-                                                <div class="section-header__arrows">
-                                                    <div class="arrow section-header__arrow section-header__arrow--prev arrow--prev">
-                                                        <button class="arrow__button" type="button">
-                                                            <svg width="7" height="11">
-                                                                <path d="M6.7,0.3L6.7,0.3c-0.4-0.4-0.9-0.4-1.3,0L0,5.5l5.4,5.2c0.4,0.4,0.9,0.3,1.3,0l0,0c0.4-0.4,0.4-1,0-1.3l-4-3.9l4-3.9C7.1,1.2,7.1,0.6,6.7,0.3z" />
-                                                            </svg>
-                                                        </button>
-                                                    </div>
-                                                    <div class="arrow section-header__arrow section-header__arrow--next arrow--next">
-                                                        <button class="arrow__button" type="button">
-                                                            <svg width="7" height="11">
-                                                                <path
-                                                                    d="M0.3,10.7L0.3,10.7c0.4,0.4,0.9,0.4,1.3,0L7,5.5L1.6,0.3C1.2-0.1,0.7,0,0.3,0.3l0,0c-0.4,0.4-0.4,1,0,1.3l4,3.9l-4,3.9
-	C-0.1,9.8-0.1,10.4,0.3,10.7z"
-                                                                />
-                                                            </svg>
-                                                        </button>
-                                                    </div>
-                                                </div>
-                                                <div class="section-header__divider"></div>
-                                            </div>
-                                        </div>
+<div class="block block-products-carousel" data-layout="grid-5">
+	<div class="container">
+		<div class="section-header">
+			<div class="section-header__body">
+				<!-- <h2 class="section-header__title"> -->
+				<?php
+					$heading = apply_filters( 'woocommerce_product_related_products_heading', __( 'Related products', 'woocommerce' ) );
+
+					if ( $heading ) :
+						?>
+						<h2><?php echo esc_html( $heading ); ?></h2>
+					<?php endif; ?>
+				<!-- </h2> -->
+				<div class="section-header__spring"></div>
+				<div class="section-header__arrows">
+					<div class="arrow section-header__arrow section-header__arrow--prev arrow--prev">
+						<button class="arrow__button" type="button">
+							<svg width="7" height="11">
+								<path d="M6.7,0.3L6.7,0.3c-0.4-0.4-0.9-0.4-1.3,0L0,5.5l5.4,5.2c0.4,0.4,0.9,0.3,1.3,0l0,0c0.4-0.4,0.4-1,0-1.3l-4-3.9l4-3.9C7.1,1.2,7.1,0.6,6.7,0.3z" />
+							</svg>
+						</button>
+					</div>
+					<div class="arrow section-header__arrow section-header__arrow--next arrow--next">
+						<button class="arrow__button" type="button">
+							<svg width="7" height="11">
+								<path
+									d="M0.3,10.7L0.3,10.7c0.4,0.4,0.9,0.4,1.3,0L7,5.5L1.6,0.3C1.2-0.1,0.7,0,0.3,0.3l0,0c-0.4,0.4-0.4,1,0,1.3l4,3.9l-4,3.9
+C-0.1,9.8-0.1,10.4,0.3,10.7z"
+								/>
+							</svg>
+						</button>
+					</div>
+				</div>
+				<div class="section-header__divider"></div>
+			</div>
+		</div>
 		<!-- new html end -->
 
 		<!-- new html 2 firt -->
-		<!-- <div class="block-products-carousel__carousel">
-			<div class="block-products-carousel__carousel-loader"></div>
-			<div class="owl-carousel"> -->
+		
 				
 		<!-- new html 2 firt end -->
 		
 		<?php woocommerce_product_loop_start(); ?>
+		<div class="block-products-carousel__carousel">
+			<div class="block-products-carousel__carousel-loader"></div>
+			<div class="owl-carousel">
 
 			<?php foreach ( $related_products as $related_product ) : ?>
+			         
 
 					<?php
 					$post_object = get_post( $related_product->get_id() );
@@ -169,12 +172,13 @@ C17.9,5.2,17.7,5,17.5,5H9.4C9.2,5,9,4.8,9,4.6V3.4C9,3.2,9.2,3,9.4,3h9.2C19.4,3,2
 					
 
 			<?php endforeach; ?>
+			</div>
+		</div>
 
 		<?php woocommerce_product_loop_end(); ?>
 
 		<!-- new html 2 second -->
-		<!-- </div>
-		</div> -->
+		
 				
 		<!-- new html 2 second end -->
         <!-- new html  -->
